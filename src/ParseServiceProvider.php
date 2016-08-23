d29ad62c0aa30043774d88c7043cd1087c6214f6
AlexCatch <alex@langleyfoxall.co.uk>
2016-08-23 09:38:14 +0100

    Updated Parse Service provider to correctly register auth provider.

--- src/ParseServiceProvider.php
+++ src/ParseServiceProvider.php
@@ -8,6 +8,8 @@ use LaraParse\Subclasses\User;
 use Parse\ParseClient;
 use LaraParse\Session\ParseSessionStorage;
 
+use Auth;
+
 class ParseServiceProvider extends ServiceProvider
 {
 
@@ -60,7 +62,7 @@ class ParseServiceProvider extends ServiceProvider
 
     private function registerAuthProvider()
     {
-        $this->app['auth']->extend('parse', function () {
+        Auth::provider('parse', function() {
             return new ParseUserProvider;
         });
     }
